31d1513e93e2f6736d292cf994d096a08072335d
001Rakib <rakibrakibrakib354@gmail.com>
2024-09-15 23:59:40 +0600

    added axios tanstack query and handled user registration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,43 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+type Role = keyof typeof RoleBasedRoutes;
+
+const AuthRoutes = ["/login", "/register"];
+const RoleBasedRoutes = {
+  USER: [/^\/profile/],
+  ADMIN: [/^\/admin/],
+};
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  //   const user = {
-  //     name: "Rakib",
-  //     token: "adashdfao32458owraht",
-  //     role: "USER",
-  //   };
+  const { pathname } = request.nextUrl;
+
+  // const user = {
+  //   // name: "Rakib",
+  //   // role: "USER",
+  // };
+
   const user = undefined;
-  if (user?.name) {
-    return NextResponse.next();
-  } else {
-    console.log("Authentication");
-    return NextResponse.redirect(new URL("/login", request.url));
+
+  if (!user) {
+    if (AuthRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+  if (user?.role && RoleBasedRoutes[user?.role as Role]) {
+    const routes = RoleBasedRoutes[user?.role as Role];
+
+    if (routes.some((route) => pathname.match(route))) {
+      return NextResponse.next();
+    }
   }
   return NextResponse.redirect(new URL("/login", request.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/profile", "/admin"],
+  matcher: ["/profile", "/admin", "/login", "/register"],
 };
